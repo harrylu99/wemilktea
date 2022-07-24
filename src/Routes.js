d8233591b65d277c60373a2ebec76e24b91428a5
harrylu99 <luhanyao@gmail.com>
2022-07-24 20:46:39 +1200

    Fixed extra import

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Layout from './HOC/Layout'
 import Home from './Components/Home'
 import Explore from './Components/Explore'
